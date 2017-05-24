78634ed955bf8d1944b54b4e50d4a88cd8e04134
Lin Sun <you@example.com>
2017-05-24 15:18:51 +0800

    1

--- E-Staff/src/com/task/MyTask.java
+++ E-Staff/src/com/task/MyTask.java
@@ -5,10 +5,10 @@ package com.task;
 public class MyTask {  
 
     public void show() {  
-        //System.out.println("show method 1");  
+        System.out.println("show method 1");  
     }  
 
     public void print() {  
-       // System.out.println("print method 1");  
+       System.out.println("print method 1");  
     }  
 }
